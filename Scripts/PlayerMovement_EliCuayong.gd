40b33d5902933a691a152c6998951b60bbcc86bb
MPLekter <65944671+MPLekter@users.noreply.github.com>
2023-03-04 22:32:03 +0100

    Add superJump logic

--- Scripts/PlayerMovement_EliCuayong.gd
+++ Scripts/PlayerMovement_EliCuayong.gd
@@ -7,6 +7,9 @@ export var gravity := 15
 export var maxFallSpeed := 140
 export var jumpForce := 500
 export var maxJumps := 2
+var jumps := 0
+export var superJump := false
+export var superJumpMultiplier := 1.99
 
 #slide related 
 export var slideTimer := 2.2
@@ -15,15 +18,15 @@ var underPlatform := false
 #movement related
 export var maxSpeed := 100
 export var accelerationForce := 20
-
-
 var motion := Vector2()
-var jumps := 0
+
+#debug related 
+var timestamp = Time.get_datetime_string_from_system()
+
 
 
 
 func _ready():
-	print_debug("ready here")
 	applyTraversePointsLogic()
 	
 func applyTraversePointsLogic():
@@ -35,24 +38,39 @@ func applyTraversePointsLogic():
 		#print_debug(traversePoint, " connected my signals")
 		#print_debug(traversePoint.is_connected("body_entered", self, "changeUnderPlatformState"))
 		
-		
-func changeUnderPlatformState(body):
-	if body == self:
-		print_debug(Time.get_datetime_string_from_system(), " STATE: under platform is now ", !underPlatform)
-		underPlatform = !underPlatform
-		if underPlatform == false:
-			$SlidingCollisionShape.set_disabled(true)
-			$StandingCollisionShape.set_disabled(false)
-			rotateSprite("standing")
-	
 func _physics_process(delta):
 	
 	applyGravity()
 	movementLogic()
 	slideLogic()
 	jumpLogic()
+	superjumpLogic()
 	applyMotion()
+		
+func changeUnderPlatformState(body):
+	if body == self:
+		print_debug(timestamp, " STATE: under platform is now ", !underPlatform)
+		underPlatform = !underPlatform
+		if underPlatform == false:
+			playerStandUp()
 
+			
+func playerStandUp():
+	$SlidingCollisionShape.set_disabled(true)
+	$StandingCollisionShape.set_disabled(false)
+	rotateSprite("standing")
+
+func playerSlide():
+	$SlidingCollisionShape.set_disabled(false)
+	$StandingCollisionShape.set_disabled(true)
+	rotateSprite("sliding")
+			
+	
+func superjumpLogic():
+	if Input.is_action_just_released("player_superjump_toggle"):
+		superJump = !superJump
+		print_debug(timestamp, " superjump is now ", superJump)
+	
 func movementLogic():
 	#move no faster than maxSpeed
 	motion.x = clamp(motion.x, -maxSpeed, maxSpeed)
@@ -71,30 +89,35 @@ func applyGravity():
 		motion.y = maxFallSpeed
 
 func slideLogic():
+	#use smaller collision shape
+	#when holding action, start slowing down gradually until no motion at all
+	#keep sliding position
+	#if underPlatform, don't do that, just keep sliding
 	var isMoving = not is_zero_approx(motion.x)
 	if is_on_floor():
 		if Input.is_action_just_pressed("player_crouch") and isMoving:
-			print_debug(Time.get_datetime_string_from_system(), " STATE: begun sliding")
-			$SlidingCollisionShape.set_disabled(false)
-			$StandingCollisionShape.set_disabled(true)
-			rotateSprite("sliding")
+			print_debug(timestamp, " STATE: begun sliding")
+			playerSlide()
 			yield (get_tree().create_timer(slideTimer), "timeout")
 			if not underPlatform:
-				print_debug(Time.get_datetime_string_from_system(), " STATE: finished sliding")
-				$SlidingCollisionShape.set_disabled(true)
-				$StandingCollisionShape.set_disabled(false)
-				rotateSprite("standing")
+				print_debug(timestamp, " STATE: finished sliding")
+				playerStandUp()
 			else:
-				print_debug(Time.get_datetime_string_from_system(), " STATE: trying to stand up but still under a platform!")
-			
+				print_debug(timestamp, " STATE: trying to stand up but still under a platform!")
 			
 func jumpLogic():
+	#2 phase animation
+	#full mobility while in air
 	if is_on_floor():
 		jumps = 0  
-	if Input.is_action_just_pressed("player_jump"): #just pressed here
-		jumps += 1
+	if Input.is_action_just_pressed("player_jump"): 
 		if jumps <= maxJumps:
-			motion.y = -jumpForce
+			if not superJump:
+				jumps += 1 #double jump if not double jumped already
+				motion.y = -jumpForce
+			else:
+				jumps = maxJumps + 1 #no double jump for superJump
+				motion.y = -jumpForce * superJumpMultiplier
 
 func applyMotion():
 	motion = move_and_slide(motion, UP)
